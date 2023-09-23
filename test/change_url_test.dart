fda48bde61cb62e961cedfc6f0d2da3e4a1203ea
drewd03 <andrew.bonner@bsu.edu>
2023-09-23 17:41:40 -0400

    Minor refactoring in the test folder

--- test/change_url_test.dart
+++ test/change_url_test.dart
@@ -5,24 +5,20 @@ import 'package:two_week_project/user_input.dart';
 void main(){
   final input = UserInputs();
   final changer = WikiBuilder();
+  final name= input.article;
+  final limit = input.limit;
+  String firstHalf = changer.nameChanger(name);
+  String secondHalf = changer.limitChanger(limit);
   test(('Page is trump'),(){
-    final name= input.article;
-    final result = changer.nameChanger(name);
-    expect(result,'https://en.wikipedia.org/w/api.php?'
+    expect(firstHalf,'https://en.wikipedia.org/w/api.php?'
         'action=query&format=json&prop='
         'revisions&titles='
         'Trump');
   });
   test (('Page Limit is 1'),(){
-    final limit = input.limit;
-    final result= changer.limitChanger(limit);
-    expect(result,'&rvprop=timestamp|user&rvlimit=1&redirects');
+    expect(secondHalf,'&rvprop=timestamp|user&rvlimit=1&redirects');
   });
   test(('This is a complete url'), (){
-    final name = input.article;
-    final limit = input.limit;
-    String firstHalf = changer.nameChanger(name);
-    String secondHalf = changer.limitChanger(limit);
     String complete = changer.builder(firstHalf, secondHalf);
     expect(complete,'https://en.wikipedia.org/w/api.php?'
         'action=query&format=json&prop='
